469f669d65fd662f93dc61c3822fa8e3a828aa9a
DanielSiew <119734350+DanielSiew@users.noreply.github.com>
2022-12-05 09:45:00 +0800

    Update day 4 pt 1.py

--- day 4/day 4 pt 1.py
+++ day 4/day 4 pt 1.py
@@ -5,10 +5,15 @@ counter = 0
 
 for line in text:
     lines = line.strip()
+    
+    # splits assignment pairs into 2 strings
     rangeString1, rangeString2 = lines.split(",")
+    
+    # splits each string into a list with starting and ending value of a range, e.g. ['3', '5'] for range 3 to 5
     rangeList1 = rangeString1.split("-")
     rangeList2 = rangeString2.split("-")
 
+    # changes the value in the list from string to integer for conditional statements later, e.g. ['3', '5'] to [3, 5]
     rangeList1 = [int(num) for num in rangeList1]
     rangeList2 = [int(num) for num in rangeList2]
 
