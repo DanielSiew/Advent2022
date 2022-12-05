01cfdbeada3ff7634870f487120080582ba36709
DanielSiew <119734350+DanielSiew@users.noreply.github.com>
2022-12-05 09:22:12 +0800

    Update day 3 pt 1.py

--- day 3/day 3 pt 1.py
+++ day 3/day 3 pt 1.py
@@ -5,9 +5,11 @@ totalPriority = 0
 
 for line in text:
     lines = line.strip()
+    
+    # splits line in text file into two to represent 2 compartments in one rucksack
     compartmentFirst = lines[slice(0, len(lines) // 2)]
     compartmentSecond = lines[slice(len(lines) // 2, len(lines))]
-    common_itemType = list(set(compartmentFirst).intersection(compartmentSecond))[0]
+    common_itemType = list(set(compartmentFirst).intersection(compartmentSecond))[0]  # gets the common character found in both compartments
 
     if common_itemType.isupper():
         priority = int(ord(common_itemType)) - 38  # returns value from 27 to 52
