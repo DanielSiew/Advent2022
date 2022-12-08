87fe24bfb2bc3943b03ef4e9722d1ed7682eb270
DanielSiew <119734350+DanielSiew@users.noreply.github.com>
2022-12-08 11:20:01 +0800

    Add files via upload

---
+++ 2022/day 6/day 6.py
@@ -0,0 +1,22 @@
+file = open("day 6.txt", "r")
+text = file.readline()
+
+
+def get_answer(part):
+    match part:
+        case 'one':
+            counter = 4
+        case 'two':
+            counter = 14
+
+    value = int(str(counter))
+
+    for index in range(len(text)):
+        if len(set(text[index:index + value])) == value:
+            break
+        counter += 1
+
+    return counter
+
+
+print(get_answer('one'), get_answer('two'))
